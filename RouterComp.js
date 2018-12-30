15dbf7e4039b0e119624db271b79c50763322ee5
sardieri <sardieri@gmail.com>
2018-12-30 21:51:36 +0300

    added router scenes in a stack

--- RouterComp.js
+++ RouterComp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Scene, Router, Actions } from 'react-native-router-flux';
+import { Scene, Router, Actions, Stack } from 'react-native-router-flux';
 import LoginForm from './src/components/LoginForm';
 import EmployeeList from './src/components/EmployeeList';
 import EmployeeCreate from './src/components/EmployeeCreate';
@@ -8,22 +8,24 @@ import EmployeeEdit from './src/components/EmployeeEdit';
 const RouterComp = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key="auth">
-        <Scene key="login" component={LoginForm} title="Please Login" />
-      </Scene>
+      <Stack key='root'>
+        <Scene key="auth">
+          <Scene key="login" component={LoginForm} title="Please Login" />
+        </Scene>
 
-      <Scene key="main">
-        <Scene
-          onRight={() => Actions.employeeCreate()}
-          rightTitle="Add"
-          key="employeeList"
-          component={EmployeeList}
-          title="Employees"
-          initial
-        />
-        <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
-        <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
-      </Scene>
+        <Scene key="main">
+          <Scene
+            onRight={() => Actions.employeeCreate()}
+            rightTitle="Add"
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+            initial
+          />
+          <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+          <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
+        </Scene>
+      </Stack>
     </Router>
   );
 };
